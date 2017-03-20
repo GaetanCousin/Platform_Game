1d1dd8c61415a0fbdc02e8399907797a06e13f2a
Gaetan Cousin <cousin.gaetan@laposte.net>
2017-03-20 17:28:56 +0100

    version commente des promise2

--- Projet_web/js/rpg.js
+++ Projet_web/js/rpg.js
@@ -1,3 +1,6 @@
+
+
+
 window.onload = function() {
 
 	var canvas = document.getElementById('canvas');
@@ -8,27 +11,82 @@ window.onload = function() {
 	var map = new Map("deuxieme", ctx);
 
 	
+ function(nom, callback){
+	// Création de l'objet XmlHttpRequest
+	let xhr = new XMLHttpRequest();
 
+	// Une fois le fichier chargé, on parse
+	xhr.addEventListener("readystatechange", function (ev) {
+	if (xhr.status == 200 && xhr.readyState == 4) {
+	        var mapDa
 
 	/*var map = new Map("deuxieme", canvas, function() {
 		console.log('this = ');
 		console.log(this);
 		console.log('map  =');
 		console.log(map);*/
+ function(nom, callback){
+	// Création de l'objet XmlHttpRequest
+	let xhr = new XMLHttpRequest();
 
-	// map.dessinerMap(ctx);
+	// Une fois le fichier chargé, on parse function(nom, callback){
+	// Création de l'objet XmlHttpRequest
+	let xhr = new XMLHttpRequest();
+
+	// Une fois le fichier chargé, on parse
+	xhr.addEventListener("readystatechange", function (ev) {
+	if (xhr.status == 200 && xhr.readyState == 4) {
+	        var mapDa function(nom, callback){
+	// Création de l'objet XmlHttpRequest
+	let xhr = new XMLHttpRequest();
+
+	// Une fois le fichier chargé, on parse
+	xhr.addEventListener("readystatechange", function (ev) {
+	if (xhr.status == 200 && xhr.readyState == 4) {
+	        var mapDa
+	xhr.addEventListener("readystatechange", function (ev) {
+	if (xhr.status == 200 && xhr.readyState == 4) {
+	        var mapDa
+	map.dessinerMap(ctx);
+	
+	/*
 	setTimeout(function(){ 
 		map.dessinerMap(ctx);
 		console.log('apres la fonction dessinerMap');
 	}, 100);
-
+*/
 	
 
 
 	//});
 
 	//setTimeout(function(){});
-
-
-
-	}
+	
+	
+	/*
+	 * 
+	 * 
+	 * function fi (di - 1 ) {
+	 *   ((Travail preparatoire ))
+		 * return  new Promise(  (ok, error) => {
+		 * 			((calcul asynchrone qui calcule di))
+		 * 			Si pas d'erreur, alors ok(di);
+		 * 			sinon error(di);
+		 * });
+	 * }
+	 * 
+	 * window.addEventListener("load", () => {
+	 * 
+		 * var start = Promise.resolve(MonArguments);
+		 *
+		 * start
+		 * 		.then (loadMap);
+		 * 		.then (loadImage);
+		 * 		.then (ImgArray => { 
+		 * 			var div = document.getElementById('images');
+		 * 			ImgArray.forEach(img) => { div.appendChild(img) ;
+		 * 		});
+		 * });
+		 * 
+	 * } 
+	 */
